8f95582275b7dc4254e6c11ccf069fd7d2f97352
zanwhip <hungnv.21it@vku.udn.vn>
2023-07-13 10:00:27 +0700

    create header components

--- src/components/Imagepost.js
+++ src/components/Imagepost.js
@@ -1,15 +1,11 @@
 import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 
-const hotnew1 = '../assets/image/hotnew1.png';
-const hotnew2 = '../assets/image/hotnew2.png';
-const hotnew3 = '../assets/image/hotnew3.png';
-
-const Imagepost = () => {
+const Imagepost = ( {image,caption}) => { 
   return (
     <View style = {styles.container}>
-       <Image source={{ uri: hotnew1 }}  style={styles.image}  />
-       <Text>Toàn cảnh Chương trình phát động InTE-UD 2023 tại ĐHĐN</Text>
+       <Image src={image} style={styles.image}  />
+      <Text>{caption}</Text>
     </View>
   )
 }
@@ -20,20 +16,22 @@ const styles = StyleSheet.create({
     container : {
         width : '100%',
         height : 253,
-        backgroundColor : '#ffffff',
+        backgroundColor : '#e3e3e3',
         marginVertical : 50,
         justifyContent : 'center',
-        
+
 
     },
     image : {
       width : '100%',
       height : 254,
+      borderRadius : 10,
 
     },
     title : {
+      //marginTop : 30,
       fontSize : 15,
-      fontWeight : 16,
+      fontWeight : '500',
       fontStyle : 'italic',
       
     }
--- src/screens/HotnewScreen.js
+++ src/screens/HotnewScreen.js
@@ -1,23 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Feather from 'react-native-vector-icons/Feather';
 import React from 'react'
 import { ScrollView } from 'react-native';
 import Imagepost from '../components/Imagepost';
+import Header from '../components/Header';
 
+const hotnew1 = '../assets/image/hotnew1.png'
+const hotnew2 = '../assets/image/hotnew2.png'
+const hotnew3 = '../assets/image/hotnew3.png'
 
-const HotnewScreen = () => {
+const caption1 = 'Toàn cảnh Chương trình phát động InTE-UD 2023 tại ĐHĐN'
+const caption2 = 'TS. Đỗ Lê Hưng Toàn-Chủ tịch Hội SV ĐHĐN chia sẻ với SV'
+const caption3 = 'Chuyên gia khởi nghiệp giao lưu,truyền cảm hứng cho SV'
+
+const openning = 'Tiếp nối thành công của hai mùa trước, Cuộc thi "Khởi nghiệp Công nghệ trong sinh viên” InTE-UD lần thứ III, “sân chơi” học thuật, sáng tạo của sinh viên (SV) do Hội SV Đại học Đà Nẵng (ĐHĐN) khởi xướng, phát động với sự đồng hành, tài trợ của Sở Khoa học và Công nghệ (KHCN) thành phố Đà Nẵng, Ngân hàng BIDV Chi nhánh Hải Vân năm nay tiếp tục được phát động, không những chỉ dành cho SV ĐHĐN mà còn lan tỏa, truyền cảm hứng cho SV các trường ĐH đổi mới sáng tạo (ĐMST) và khởi nghiệp.'
+
+const title1 = ' Tham dự Chương trình phát động có Bà Lê Thị Thục-Phó Giám đốc Sở KHCN thành phố Đà Nẵng; Ông Lê Công Hùng-Phó Chủ tịch Trung ương Hội SV Việt Nam, Phó Bí thư Thành Đoàn, Chủ tịch Hội SV thành phố Đà Nẵng; PGS.TS. Lê Thành Bắc-Phó Giám đốc ĐHĐN; đại diện lãnh đạo các đơn vị, doanh nghiệp đối tác đồng hành; đại diện lãnh đạo các ban hữu quan và các trường ĐH thành viên của ĐHĐN cùng các chuyên gia và các nhóm SV đăng ký tham dự, quan tâm đến Cuộc thi'
+const title2 = 'Phát biểu tại Chương trình phát động InTE-UD 2023, TS. Đỗ Lê Hưng Toàn-Chủ tịch Hội SV ĐHĐN, Trưởng Ban Tổ chức Cuộc thi cho biết, Cuộc thi năm nay nhằm tìm kiếm những hạt nhân, ý tưởng sáng tạo và khởi nghiệp trong SV, qua đó hỗ trợ, đồng hành với những ước mơ, khát vọng khởi nghiệp trẻ trong tương lai.'
+
+const header = 'Tin tức - Sự kiện '
+
+const HotnewScreen = ( image, caption , header) => {
   return (
     <View style={styles.container}>
-    <View style={styles.header}>
-    <Ionicons 
-    name="arrow-back-outline"
-    size={20}
-    color='#ffffff'
-    onPress={() => navigation.goBack()}
-    />
-    <Text style={styles.textheader}>Tin tức - Sự kiện</Text>
-     </View>
+    <Header header={header} />
      <ScrollView style={styles.displaycontainer}>
         <View style={styles.caption}>
             <Text style={styles.captiontext}>Phát động Cuộc thi “Khởi nghiệp Công nghệ trong sinh viên” InTE-UD lần thứ III: Góp phần kiến tạo cộng đồng khởi nghiệp trẻ từ giảng đường</Text>
@@ -31,13 +38,13 @@ const HotnewScreen = () => {
             <Text style={styles.datetext}>09/07/2023</Text>
         </View>
         <View style={styles.openning}>
-        <Text style={styles.openningtext}>Tiếp nối thành công của hai mùa trước, Cuộc thi "Khởi nghiệp Công nghệ trong sinh viên” InTE-UD lần thứ III, “sân chơi” học thuật, sáng tạo của sinh viên (SV) do Hội SV Đại học Đà Nẵng (ĐHĐN) khởi xướng, phát động với sự đồng hành, tài trợ của Sở Khoa học và Công nghệ (KHCN) thành phố Đà Nẵng, Ngân hàng BIDV Chi nhánh Hải Vân năm nay tiếp tục được phát động, không những chỉ dành cho SV ĐHĐN mà còn lan tỏa, truyền cảm hứng cho SV các trường ĐH đổi mới sáng tạo (ĐMST) và khởi nghiệp.</Text>
+        <Text style={styles.openningtext}>{openning}</Text>
         </View>
-        <Imagepost />
-        <Text>Tham dự Chương trình phát động có Bà Lê Thị Thục-Phó Giám đốc Sở KHCN thành phố Đà Nẵng; Ông Lê Công Hùng-Phó Chủ tịch Trung ương Hội SV Việt Nam, Phó Bí thư Thành Đoàn, Chủ tịch Hội SV thành phố Đà Nẵng; PGS.TS. Lê Thành Bắc-Phó Giám đốc ĐHĐN; đại diện lãnh đạo các đơn vị, doanh nghiệp đối tác đồng hành; đại diện lãnh đạo các ban hữu quan và các trường ĐH thành viên của ĐHĐN cùng các chuyên gia và các nhóm SV đăng ký tham dự, quan tâm đến Cuộc thi.</Text>
-        <Imagepost />
-        <Text>Phát biểu tại Chương trình phát động InTE-UD 2023, TS. Đỗ Lê Hưng Toàn-Chủ tịch Hội SV ĐHĐN, Trưởng Ban Tổ chức Cuộc thi cho biết, Cuộc thi năm nay nhằm tìm kiếm những hạt nhân, ý tưởng sáng tạo và khởi nghiệp trong SV, qua đó hỗ trợ, đồng hành với những ước mơ, khát vọng khởi nghiệp trẻ trong tương lai.</Text>
-        <Imagepost />   
+        <Imagepost image={hotnew1} caption={caption1} />
+        <Text>{title1}</Text>
+        <Imagepost image={hotnew2} caption={caption2} />
+        <Text>{title2}</Text>
+        <Imagepost image={hotnew3} caption={caption3}/>  
      </ScrollView>
     </View>
   )
@@ -46,33 +53,16 @@ const HotnewScreen = () => {
 export default HotnewScreen
 
 const styles = StyleSheet.create({
-    container : {
-        flex: 1,
-        backgroundColor: '#e3e3e3',
-        
-    },
-    header : {
-      flexDirection : 'row',
-      width : '100%',
-      height : 60,
-      backgroundColor : '#1668C7',
-      top : 0,
-      paddingTop : 20,
-      alignContent : 'center',
-      //paddingHorizontal : 10,
+  container : {
+    flex: 1,
+    backgroundColor: '#e3e3e3',
+    
+},
+displaycontainer : {
+  width : '100%',
+  paddingHorizontal : 20,
 
-  },
-  displaycontainer : {
-    width : '100%',
-    paddingHorizontal : 20,
-
-  },
-  textheader : {
-      fontSize : 22,
-      color : '#ffffff',
-      fontWeight : 'bold',
-      marginLeft : 80,
-  },
+},
   caption : {
     width : '100%',
     marginTop : 10,
